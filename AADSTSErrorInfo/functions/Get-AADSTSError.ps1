b671152d5a5967a25a046460a17db6b8abd381b5
alex.verboon <verboonalex@gmail.com>
2021-01-18 17:58:42 +0100

    hello world

---
+++ AADSTSErrorInfo/functions/Get-AADSTSError.ps1
@@ -0,0 +1,75 @@
+function Get-AADSTSError
+{
+<#
+.Synopsis
+   Get-AADSTSError
+.DESCRIPTION
+   Get-AADSTSError performs a lookup for the specified AADSTS error code and returns  error code description and remeidiation information
+.PARAMETER ErrorCode
+    The AADSTS error code
+
+    https://docs.microsoft.com/en-us/azure/active-directory/develop/reference-aadsts-error-codes#aadsts-error-codes
+
+.EXAMPLE
+   Get-AADSTSError -ErrorCode AADSTS50076
+
+   ErrorCode   : 50076
+   Description : Due to a configuration change made by your administrator, or because you moved to a new location, you must use multi-factor 
+                 authentication to access '{resource}'.
+    Remediation: User needs to perform multi-factor authentication. There could be multiple things requiring multi-factor, e.g. Conditional Access 
+                 policies, per-user enforcement, requested by client, among others.
+
+#>
+
+    [CmdletBinding()]
+    Param
+    (
+        # The AADSTS Error code to lookup
+        [Parameter(Mandatory=$true)]
+        [string]$ErrorCode
+    )
+
+    Begin
+    {
+        $Data = [System.Collections.ArrayList]::new()
+        $uri = "https://login.microsoftonline.com/error"
+        $body = "code=$ErrorCode"
+
+    }
+    Process
+    {
+        Try{
+        $request = Invoke-WebRequest -Uri $uri -Body $body -Method Post 
+        $result = @(Get-AADSTSHtmlInfo -page $request)
+        If($request.RawContent -like "*Code not found!*")
+        {
+            Write-Verbose "Error code: $ErrorCode not found" 
+            $object = [PSCustomObject]@{
+                ErrorCode   = $ErrorCode
+                Description = "not found"
+                Remediation = ""
+            }
+            [void]$Data.Add($object)
+        }
+        Else
+        {
+            $object = [PSCustomObject]@{
+                ErrorCode   = (($result.0).1)[0]
+                Description = (($result.0).1)[1]
+                Remediation = If ([string]::IsNullOrEmpty( (($result.0).1)[2])) {""} Else { (($result.0).1)[2] }
+            }
+            [void]$Data.Add($object)
+        }
+        $Data 
+    }
+    Catch{
+        Write-Error "Error retrieving AADSTS Error information [$_]"
+    }  
+    }
+    End
+    {
+    }
+}
+
+
+                       
